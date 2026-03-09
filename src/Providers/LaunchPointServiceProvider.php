7970ae4a128c5eee43b48deabc82b29e2bbb6299
khaledAbdalbasit <kahledabdelbasit12@gmail.com>
2026-03-09 23:46:29 +0200

    Initial commit with launchpoint package and README

--- src/Providers/LaunchPointServiceProvider.php
+++ src/Providers/LaunchPointServiceProvider.php
@@ -14,31 +14,44 @@ class LaunchPointServiceProvider extends ServiceProvider
 
     public function boot()
     {
-
+        // Publish config
         $this->publishes([
             __DIR__ . '/../../config/launchpoint.php' => config_path('launchpoint.php'),
         ], 'config');
 
+        // Publish ApiResponse Trait
         $traitPath = app_path('Traits/ApiResponseTrait.php');
         if (!File::exists($traitPath)) {
             File::ensureDirectoryExists(app_path('Traits'));
-            File::put($traitPath, file_get_contents(__DIR__ . '/stubs/ApiResponseTrait.stub'));
+            File::put(
+                $traitPath,
+                file_get_contents(base_path('vendor/khaledabdalbasit/launchpoint/src/stubs/ApiResponseTrait.stub'))
+            );
         }
 
+        // Publish Controllers
         $controllers = ['SettingsController', 'AuthController', 'ProfileController'];
         foreach ($controllers as $controller) {
             $controllerPath = app_path("Http/Controllers/Api/{$controller}.php");
             if (!File::exists($controllerPath)) {
                 File::ensureDirectoryExists(app_path('Http/Controllers/Api'));
-                File::put($controllerPath, file_get_contents(__DIR__ . "/../stubs/{$controller}.stub"));
+                File::put(
+                    $controllerPath,
+                    file_get_contents(__DIR__ . "/../stubs/{$controller}.stub")
+                );
             }
         }
 
+        // Publish API routes
         $routesPath = base_path('routes/api.php');
         if (!str_contains(file_get_contents($routesPath), 'LaunchPoint Routes')) {
-            File::append($routesPath, file_get_contents(__DIR__ . '/../stubs/api_routes.stub'));
+            File::append(
+                $routesPath,
+                file_get_contents(__DIR__ . '/../stubs/api_routes.stub')
+            );
         }
 
+        // Optional: Global Exception Handler snippet
         $handlerPath = app_path('Exceptions/Handler.php');
         if (File::exists($handlerPath)) {
             $content = File::get($handlerPath);
