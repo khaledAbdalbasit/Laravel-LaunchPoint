93f78d4ad6b308c8d5ad595a376f61990bce21f1
khaledAbdalbasit <kahledabdelbasit12@gmail.com>
2026-03-09 22:36:17 +0200

    Initial commit with launchpoint package and README

--- src/Providers/LaunchPointServiceProvider.php
+++ src/Providers/LaunchPointServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Providers;
+namespace KhaledAbdalbasit\LaunchPoint\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\File;
@@ -9,21 +9,25 @@ class LaunchPointServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__ . '/../../config/launchpoint.php', 'launchpoint');
+        // Merge config from package
+        $this->mergeConfigFrom(__DIR__ . '/../config/launchpoint.php', 'launchpoint');
     }
 
     public function boot()
     {
         // Publish config
         $this->publishes([
-            __DIR__ . '/../../config/launchpoint.php' => config_path('launchpoint.php'),
+            __DIR__ . '/../config/launchpoint.php' => config_path('launchpoint.php'),
         ], 'config');
 
         // Publish ApiResponse Trait
         $traitPath = app_path('Traits/ApiResponseTrait.php');
         if (!File::exists($traitPath)) {
             File::ensureDirectoryExists(app_path('Traits'));
-            File::put($traitPath, file_get_contents(__DIR__ . '/../../stubs/ApiResponseTrait.stub'));
+            File::put(
+                $traitPath,
+                file_get_contents(__DIR__ . '/../stubs/ApiResponseTrait.stub')
+            );
         }
 
         // Publish Controllers
@@ -32,14 +36,20 @@ class LaunchPointServiceProvider extends ServiceProvider
             $controllerPath = app_path("Http/Controllers/Api/{$controller}.php");
             if (!File::exists($controllerPath)) {
                 File::ensureDirectoryExists(app_path('Http/Controllers/Api'));
-                File::put($controllerPath, file_get_contents(__DIR__ . "/../../stubs/{$controller}.stub"));
+                File::put(
+                    $controllerPath,
+                    file_get_contents(__DIR__ . "/../stubs/{$controller}.stub")
+                );
             }
         }
 
         // Publish API routes
         $routesPath = base_path('routes/api.php');
         if (!str_contains(file_get_contents($routesPath), 'LaunchPoint Routes')) {
-            File::append($routesPath, file_get_contents(__DIR__ . '/../../stubs/api_routes.stub'));
+            File::append(
+                $routesPath,
+                file_get_contents(__DIR__ . '/../stubs/api_routes.stub')
+            );
         }
 
         // Optional: Global Exception Handler snippet
@@ -47,7 +57,7 @@ class LaunchPointServiceProvider extends ServiceProvider
         if (File::exists($handlerPath)) {
             $content = File::get($handlerPath);
             if (!str_contains($content, 'LaunchPoint Exception Handling')) {
-                $renderable = __DIR__ . '/../../stubs/handler_renderable.stub';
+                $renderable = __DIR__ . '/../stubs/handler_renderable.stub';
                 if (File::exists($renderable)) {
                     $renderableContent = file_get_contents($renderable);
                     $content = str_replace('// register renderable here', $renderableContent, $content);
